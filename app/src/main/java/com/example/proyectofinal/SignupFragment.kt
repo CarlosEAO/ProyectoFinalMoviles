c149cda521c2682b9f2b62583efc7e7dd8774662
samisuras <raulsamuelleos98@gmail.com>
2019-12-14 14:03:29 -0600

    Diseno de signup fragment

--- app/src/main/java/com/example/proyectofinal/SignupFragment.kt
+++ app/src/main/java/com/example/proyectofinal/SignupFragment.kt
@@ -7,14 +7,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
+import kotlinx.android.synthetic.main.fragment_signup.*
 
 /**
  * A simple [Fragment] subclass.
