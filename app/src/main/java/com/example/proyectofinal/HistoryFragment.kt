a2f758c90ef5fe85ab61f61f336b39cf7e7c51ed
Carlos Aranda <carlos.eao15@gmail.com>
2019-12-16 09:18:06 -0600

    Se obtuvo arreglo de consultas ordenadas por fecha

--- app/src/main/java/com/example/proyectofinal/HistoryFragment.kt
+++ app/src/main/java/com/example/proyectofinal/HistoryFragment.kt
@@ -6,11 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 /**
  * A simple [Fragment] subclass.
  */
 class HistoryFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,5 +23,32 @@ class HistoryFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_history, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+        val db = FirebaseFirestore.getInstance()
+        val settings = FirebaseFirestoreSettings.Builder()
+            .setPersistenceEnabled(true)
+            .build()
+        db.firestoreSettings = settings
+
+        Toast.makeText(activity,auth.currentUser!!.email.toString(), Toast.LENGTH_SHORT).show()
+
+        val consultations = db.collection("consultations")
+        consultations
+            .whereEqualTo("userEmail",auth.currentUser!!.email.toString())
+            .get()
+            .addOnSuccessListener { documents ->
+                Toast.makeText(activity,documents.size().toString(), Toast.LENGTH_SHORT).show()
+                for(document in documents){
+                    Toast.makeText(activity,document.data.get("date").toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(activity,"error", Toast.LENGTH_SHORT).show()
+            }
+
     }
 }
